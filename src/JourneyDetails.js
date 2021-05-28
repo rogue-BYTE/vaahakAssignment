8d835cfa2eb409f02d6c55c6cfc77ff4c5ca277c
rogue-BYTE <vedantanandva28@gmail.com>
2021-05-28 15:50:57 +0530

    commit

--- src/JourneyDetails.js
+++ src/JourneyDetails.js
@@ -1,7 +1,60 @@
-import React from 'react';
-import {Formik, Form} from 'formik';
-import Header from './Header.js'
+import React, {useState}from 'react';
+import {Formik, Form } from 'formik';
+import Header from './Header';
 import * as Yup from 'yup';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import Textfield from './FormsUI/TextfieldWrapper';
+import Select from './FormsUI/Select';
+
+const useStyles = makeStyles(theme => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: theme.spacing(4),
+  },
+  button: {
+    backgroundColor: 'blue',
+    color: 'white',
+    margin: theme.spacing(1),
+    width: 300
+  },
+  field: {
+    margin: theme.spacing(1),
+    width: 175
+  },
+  fullWidthField: {
+    margin: theme.spacing(1),
+    width: 350,
+  },
+  dropDown: {
+    margin: theme.spacing(1),
+    width: 350
+  }
+}));
+
+const validationSchema = Yup.object().shape({
+  source: Yup
+    .string()
+    .required('Source is required'),
+  destination: Yup
+    .string()
+    .required('Destination is required'),
+  vehicleType: Yup
+    .string()
+    .required('Please select a Vehicle Type'),
+  numPassengers: Yup.number()
+    .integer()
+    .required('Please select the number of passengers')
+    .when("vehicleType", (vehicleType) => {
+      if (vehicleType=='SUV')
+        return Yup.number().integer().max(6, 'Capacity is 6').min(1);
+      else
+        return Yup.number().integer().max(4, 'Capacity is 4').min(1);
+    })
+});
 
 export const JourneyDetails = ({ formData, setFormData, nextStep }) => {
   const classes = useStyles();
